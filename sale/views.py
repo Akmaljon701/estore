7353abc7869ac3053f5aa5570816643587d5142d
Akmaljon701 <akmalkhanever@gmail.com>
2024-05-22 18:09:06 +0500

    first commit

--- sale/views.py
+++ sale/views.py
@@ -20,34 +20,31 @@ def get_categories(request):
 
 
 @swagger_auto_schema(method='GET', manual_parameters=[
-    openapi.Parameter('name', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=False),
-    openapi.Parameter('CPU', openapi.IN_QUERY, type=openapi.TYPE_INTEGER, required=False),
-    openapi.Parameter('GPU', openapi.IN_QUERY, type=openapi.TYPE_INTEGER, required=False),
-    openapi.Parameter('RAM', openapi.IN_QUERY, type=openapi.TYPE_INTEGER, required=False),
+    openapi.Parameter('category', openapi.IN_QUERY, type=openapi.TYPE_INTEGER, required=False),
 ])
 @api_view(['GET'])
 # @permission_classes([IsAuthenticated])
 # @renderer_classes([JSONRenderer])
 def get_products(request):
-    name = request.query_params.get('name')
-    CPU = request.query_params.get('CPU')
-    GPU = request.query_params.get('GPU')
-    RAM = request.query_params.get('RAM')
+    category = request.query_params.get('category')
 
     products = Product.objects.all()
 
-    filter_params = {}
-    if name:
-        filter_params['name'] = name
-    if CPU:
-        filter_params['CPU'] = CPU
-    if GPU:
-        filter_params['GPU'] = GPU
-    if RAM:
-        filter_params['RAM'] = RAM
+    if category:
+        products = products.filter(category=category)
 
-    if filter_params:
-        products = products.filter(**filter_params)
+    # filter_params = {}
+    # if name:
+    #     filter_params['name'] = name
+    # if CPU:
+    #     filter_params['CPU'] = CPU
+    # if GPU:
+    #     filter_params['GPU'] = GPU
+    # if RAM:
+    #     filter_params['RAM'] = RAM
+    #
+    # if filter_params:
+    #     products = products.filter(**filter_params)
 
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data, status=200)
